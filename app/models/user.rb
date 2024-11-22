2e27a7d44fbc02d593b62234d9908d902e17d500
Ryosuke1020sce <lickey622saito@gmail.com>
2024-11-22 16:42:47 +0900

    ユーザー情報：バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,21 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname , presence: true
-  validates :family_name , presence: true
-  validates :first_name , presence: true
-  validates :family_name_kana , presence: true
-  validates :first_name_kana , presence: true
-  validates :birthday , presence: true
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name
+    validates :family_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヴ一ー]+\z/, message: '全角カナ文字を使用してください' } do
+    validates :first_name_kana
+    validates :family_name_kana
+  end
+
+  validates :password, length: {minimum: 6}
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+	validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+
+  validates :nickname, presence: true
+  validates :birthday, presence: true
 
 end
