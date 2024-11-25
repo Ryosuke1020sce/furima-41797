836effbc42a06ea02de7b9b64a623a3c52e731a0
Ryosuke1020sce <lickey622saito@gmail.com>
2024-11-25 14:38:36 +0900

    テスト項目の修正
    ・重複e-mail登録不可
    ・パスワードの全角使用不可
    ・姓名：空では登録不可（記述済）
    ・姓名カナ：空では登録不可

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -23,6 +23,13 @@ RSpec.describe User, type: :model do
         @user.valid?
 	      expect(@user.errors.full_messages).to include("Email can't be blank")
       end
+      it '重複したemailは登録できない' do
+        @user.save
+        @another_user = FactoryBot.build(:user)
+        @another_user.email = @user.email
+        @another_user.valid?
+	      expect(@another_user.errors.full_messages).to include("Email has already been taken")
+      end
       it 'passwordが空では登録できない' do
         @user.password = ""
         @user.valid?
@@ -33,6 +40,12 @@ RSpec.describe User, type: :model do
         @user.valid?
 	      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
+      it 'passwordに全角を使用しては登録できない' do
+        @user.password = "パスワードa1"
+        @user.password_confirmation = "パスワードa1"
+        @user.valid?
+	      expect(@user.errors.full_messages).to include("Password には英字と数字の両方を含めて設定してください")
+      end      
       it 'family_nameが空では登録できない' do
         @user.family_name = ""
         @user.valid?
@@ -42,7 +55,17 @@ RSpec.describe User, type: :model do
         @user.first_name = ""
         @user.valid?
 	      expect(@user.errors.full_messages).to include("First name can't be blank")
+      end    
+      it 'family_name_kanaが空では登録できない' do
+        @user.family_name_kana = ""
+        @user.valid?
+	      expect(@user.errors.full_messages).to include("Family name kana can't be blank")
       end
+      it 'first_name_kanaが空では登録できない' do
+        @user.first_name_kana = ""
+        @user.valid?
+	      expect(@user.errors.full_messages).to include("First name kana can't be blank")
+      end    
       it 'birthdayが空では登録できない' do        
         @user.birthday = ""
         @user.valid?
