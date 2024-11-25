d68d7e29593a56bfbb8431e6df1707126f954529
Ryosuke1020sce <lickey622saito@gmail.com>
2024-11-25 16:52:13 +0900

    ActiveHashの導入等
    ・items テーブルの作成
    ・ActiveHash の導入
    ・選択肢の作成

---
+++ app/models/delivery_day.rb
@@ -0,0 +1,8 @@
+class DeliveryDay < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '---' },
+    { id: 0, name: '１～２日で発送' },
+    { id: 0, name: '２～３日で発送' },
+    { id: 0, name: '４～７日で発送' }
+  ]
+ end
