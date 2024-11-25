17ee291ac46229230e189baf61566b26d66a86c5
Ryosuke1020sce <lickey622saito@gmail.com>
2024-11-25 18:26:42 +0900

    item モデルへのバリデーションの設定
    及び、出品画面へのエラーメッセージの表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,4 +9,8 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  validates :name, :explain, :image, :price, presence: true 
+  validates :category_id, :condition_id, :delivery_fee_id, :prefecture_id,
+    :delivery_day_id, numericality: { other_than: 1, message: "can't be blank"}
+
 end
