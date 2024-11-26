33bfb4041a8571994d2f89513f02a4067b7e4062
Ryosuke1020sce <lickey622saito@gmail.com>
2024-11-26 15:55:43 +0900

    itemテーブル：userカラムのnotnull制約追加

--- db/schema.rb
+++ db/schema.rb
@@ -48,7 +48,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_25_090234) do
     t.integer "prefecture_id", null: false
     t.integer "delivery_day_id", null: false
     t.integer "price", null: false
-    t.bigint "user_id"
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_items_on_user_id"
